cc71306fe0ef40fa981d02d895b0bc269a03362b
Kamiesha Davis <mskamiesha@gmail.com>
2019-07-02 16:57:01 -0500

    psuedo code

---
+++ assets/game.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+ console.log("ready")
+})
+
+
+var computerGuess; 
+var wins
+var losses
+var totalScore
+
+computerGuess=(Math.floor(Math.random() * ((120 - 19) + 1)) + 19)
+console.log("computerGuess" +  computerGuess)
+
+
+//Random generated cpu number
+//console log
+//link to html
+
+//Random generate number for each crystal 
+// assign on click 
+//console log 
+//link to image
+
+//Add crystal numbers to total score
+//console log 
+// display total score
+
+// if crystal number matches cpu number 
+// add win
+   // reset game
+
+// if crystal numbers total are more the cpu number 
+// add loss
+   // reset game *keep losses total 
+
+
+
+
+
+
