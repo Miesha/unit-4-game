1f0c540cff25fe0d33f6d8b4672ded921ee5dabf
Kamiesha Davis <mskamiesha@gmail.com>
2019-07-02 18:19:54 -0500

    all random number generators are working

--- assets/game.js
+++ assets/game.js
@@ -4,26 +4,45 @@ $(document).ready(function(){
 
 
 var computerGuess; 
+var gem1;
+var gem2;
+var gem3;
+var gem4;
 var wins
 var losses
 var totalScore
 
 computerGuess=(Math.floor(Math.random() * ((120 - 19) + 1)) + 19)
-console.log("computerGuess" +  computerGuess)
+console.log("computerGuess : " +  computerGuess)
+
+gem1=(Math.floor(Math.random() * ((30 - 5) + 1)) + 5)
+console.log("gem1 : " + gem1)
+
+gem2=(Math.floor(Math.random() * ((15 - 4) + 1)) + 4)
+console.log("gem2 : " + gem2)
+
+gem3=(Math.floor(Math.random() * ((20 - 2) + 1)) + 2)
+console.log("gem3 : " + gem3)
+
+gem4=(Math.floor(Math.random() * ((10 - 1) + 1)) + 1)
+console.log("gem4 : " + gem4)
 
 
-//Random generated cpu number
-//console log
-//link to html
+
+//  done Random generated cpu number
+//  done console log
+//  done link to html
 
 //Random generate number for each crystal 
 // assign on click 
 //console log 
 //link to image
 
+// on click add 
 //Add crystal numbers to total score
 //console log 
 // display total score
+
 
 // if crystal number matches cpu number 
 // add win
